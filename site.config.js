bc7ce5157aedd1884e8d6d476f2f4c2a52cb5ac2
siam3310 <164713293+siam3310@users.noreply.github.com>
2024-04-14 12:51:56 +0600

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "SIAM",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Student",
+    email: "mahamudunn1005@gmail.com",
+    github: "siam3310",
   },
   projects: [
     {
@@ -23,7 +20,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://simoviesblog.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
